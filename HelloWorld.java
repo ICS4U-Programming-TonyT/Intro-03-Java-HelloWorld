2919b3a560349827a0fff23837c4248f06383451
Tony Tran <tony.tran1@stu.ocsb.ca>
2025-02-04 15:24:12 +0000

    Added HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+class Test
+{
+    public static void main(String[] args)
+    {
+        // Printing Hello, Devin. When are you getting online.
+        System.out.println("Hello, Devin. When are you getting online.");
+    }
+}
