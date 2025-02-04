eb9409b5ddadb047656c900961f6cd501c79e9a0
Tony Tran <tony.tran1@stu.ocsb.ca>
2025-02-04 15:51:12 +0000

    Add package-info.java

---
+++ com/example/package-info.java
@@ -0,0 +1,21 @@
+/**
+ * This package contains classes for performing various string operations.
+ *
+ * <p>The classes in this package provide functionality for tasks such as string manipulation,
+ *
+ * <p>searching, and parsing.
+ *
+ * <p>To use the classes in this package, simply import the package using the import statement:
+ *
+ * <pre>
+ *
+ * import com.example;
+ *
+ * </pre>
+ *
+ * <p>For more information on the classes and their usage, see the documentation for each class.
+ *
+ * @since 1.0
+ * @version 1.0
+ */
+package com.example;
