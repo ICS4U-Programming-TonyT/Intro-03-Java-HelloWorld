12d909fa3328e7683b93e2dca1d4f100929c2a87
Tony Tran <tony.tran1@stu.ocsb.ca>
2025-02-04 15:54:28 +0000

    Move HelloWorld.java

--- HelloWorld.java
+++
@@ -1,8 +0,0 @@
-class Test
-{
-    public static void main(String[] args)
-    {
-        // Printing Hello, Devin. When are you getting online.
-        System.out.println("Hello, Devin. When are you getting online.");
-    }
-}
---
+++ com/example/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.example;
+
+class Test {
+  public static void main(String[] args) {
+    // Printing Hello, Devin. When are you getting online.
+    System.out.println("Hello, Devin. When are you getting online.");
+  }
+}
